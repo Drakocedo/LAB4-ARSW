337f0a5f691722cd2673d3c78528a0cbe539d995
unknown <david.caycedo@labinfo.is.escuelaing.edu.co>
2020-02-18 15:59:33 -0500

    tests

--- BLUEPRINTS-PART2/src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
+++ BLUEPRINTS-PART2/src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
@@ -1,26 +1,137 @@
 package edu.eci.arsw.blueprints.test.services;
 
-import static org.junit.Assert.assertEquals;
-
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
+import edu.eci.arsw.blueprints.persistence.impl.InMemoryBlueprintPersistence;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest()
+/**
+ *
+ * @author hcadavid
+ */
 public class ApplicationServicesTests {
+	
 
+	
+	
     
-    //RestaurantOrderServicesStub ros;
+    @Test
+    public void saveNewAndLoadTest() throws BlueprintPersistenceException, BlueprintNotFoundException{
+        InMemoryBlueprintPersistence ibpp=new InMemoryBlueprintPersistence();
 
+        Point[] pts0=new Point[]{new Point(40, 40),new Point(15, 15)};
+        Blueprint bp0=new Blueprint("mack", "mypaint",pts0);
+        
+        ibpp.saveBlueprint(bp0);
+        
+        Point[] pts=new Point[]{new Point(0, 0),new Point(10, 10)};
+        Blueprint bp=new Blueprint("john", "thepaint",pts);
+        
+        ibpp.saveBlueprint(bp);
+        
+        assertNotNull("Loading a previously stored blueprint returned null.",ibpp.getBlueprint(bp.getAuthor(), bp.getName()));
+        
+        assertEquals("Loading a previously stored blueprint returned a different blueprint.",ibpp.getBlueprint(bp.getAuthor(), bp.getName()), bp);
+        
+    }
+
+    @Test
+    public void saveExistingBpTest() {
+        InMemoryBlueprintPersistence ibpp=new InMemoryBlueprintPersistence();
+        
+        Point[] pts=new Point[]{new Point(0, 0),new Point(10, 10)};
+        Blueprint bp=new Blueprint("john", "thepaint",pts);
+        
+        try {
+            ibpp.saveBlueprint(bp);
+        } catch (BlueprintPersistenceException ex) {
+            fail("Blueprint persistence failed inserting the first blueprint.");
+        }
+        
+        Point[] pts2=new Point[]{new Point(10, 10),new Point(20, 20)};
+        Blueprint bp2=new Blueprint("john", "thepaint",pts2);
+
+        try{
+            ibpp.saveBlueprint(bp2);
+            fail("An exception was expected after saving a second blueprint with the same name and autor");
+        }
+        catch (BlueprintPersistenceException ex){
+            
+        }
+                
+        
+    }
     
-    /* @Test
-    public void contextLoads() { 
-        assertEquals(true, true);
-    } */
+    @Test
+    public void getBluePrintTest() throws BlueprintPersistenceException, BlueprintNotFoundException{
+                
+                  
+        InMemoryBlueprintPersistence ibpp=new InMemoryBlueprintPersistence();
+
+        Point[] pts0=new Point[]{new Point(40, 40),new Point(15, 15)};
+        Blueprint bp0=new Blueprint("mack", "mypaint",pts0);
+        
+        ibpp.saveBlueprint(bp0);               
+        
+        Blueprint bp1 = ibpp.getBlueprint("mack", "mypaint");     
+        
+        
+        assertEquals("Cargo un blueprint distinto.",bp0,bp1);
+        
+    }
+
+    @Test
+    public void getBluePrintByAuthorTest() throws BlueprintPersistenceException, BlueprintNotFoundException{
+
+        InMemoryBlueprintPersistence ibpp=new InMemoryBlueprintPersistence();
+        
+        Point[] pts=new Point[]{new Point(0, 0),new Point(10, 10)};
+        Blueprint bp=new Blueprint("john", "pantera",pts);
+        
+        Point[] pts0=new Point[]{new Point(40, 40),new Point(15, 15)};
+        Blueprint bp0=new Blueprint("mack", "mypaint",pts0);
+        
+        Point[] pts2=new Point[]{new Point(10, 10),new Point(20, 20)};
+        Blueprint bp2=new Blueprint("john", "thepaint",pts2);
+        
+        try {
+            ibpp.saveBlueprint(bp);
+            ibpp.saveBlueprint(bp0);
+            ibpp.saveBlueprint(bp2);
+        } catch (BlueprintPersistenceException ex) {
+            fail("Blueprint persistence failed inserting the first blueprint.");
+        }
+        
+      
+
+        try{
+            ibpp.saveBlueprint(bp);
+            ibpp.saveBlueprint(bp0);
+            ibpp.saveBlueprint(bp2);
+            fail("An exception was expected after saving a second blueprint with the same name and autor");
+        }
+        catch (BlueprintPersistenceException ex){
+            
+        }
+        Set<Blueprint> zed = new HashSet<>();
+        zed.add(bp);
+        zed.add(bp2);
+        
+        assertEquals("Cargo un blueprint distinto.",zed,ibpp.getBlueprintsByAuthor("john"));
+
+        
+    }
+    
+    
+    
+    
+    
+    
 }
